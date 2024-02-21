df9191ab005bdd0f487dd8967917633b18f71ee3
Rajath <acharya.drajath@gmail.com>
2024-02-21 15:54:46 +0530

    67. Server side form validation II

--- internal/models/templatedata.go
+++ internal/models/templatedata.go
@@ -2,11 +2,11 @@ package models
 
 import "github.com/rajath002/bookings/internal/forms"
 
-// TemplateData holds data send from handlers to template
+// TemplateData holds data sent from handlers to templates
 type TemplateData struct {
 	StringMap map[string]string
-	IntMap    map[string]string
-	FloatMap  map[string]string
+	IntMap    map[string]int
+	FloatMap  map[string]float32
 	Data      map[string]interface{}
 	CSRFToken string
 	Flash     string
