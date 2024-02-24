f1e5eb62d3fc8c7e96e976d726f64ea26a631c5d
Rajath <acharya.drajath@gmail.com>
2024-02-24 13:03:08 +0530

    71. Finishing up our response to user, and adding alerts

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -155,8 +155,12 @@ func (m *Repository) ReservationSummary(w http.ResponseWriter, r *http.Request) 
 	reservation, ok := m.App.Session.Get(r.Context(), "reservation").(models.Reservation)
 	if !ok {
 		log.Println("Cannot get item from the session")
+		m.App.Session.Put(r.Context(), "error", "Cannot get reservation from session")
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
 		return
 	}
+
+	m.App.Session.Remove(r.Context(), "reservation")
 
 	data := make(map[string]interface{})
 	data["reservation"] = reservation
