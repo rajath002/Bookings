3b85d70eed9d420feb76be8ee87eefe148bfbbdd
Rajath <acharya.drajath@gmail.com>
2024-06-12 11:03:13 +0530

    104. Setting up database functions: inserting a reservation

--- internal/repository/dbrepo/postgres.go
+++ internal/repository/dbrepo/postgres.go
@@ -1,5 +1,37 @@
 package dbrepo
 
+import (
+	"context"
+	"time"
+
+	"github.com/rajath002/bookings/internal/models"
+)
+
 func (m *postgresDBRepo) AllUsers() bool {
 	return true
+}
+
+// Insert a reservation into the database
+func (m *postgresDBRepo) InsertReservation(res models.Reservation) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	stmt := `INSERT INTO RESERVATIONS (first_name, last_name, email, phone, start_date, end_date, room_id, created_at, updated_at) 
+			VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)`
+
+	_, err := m.DB.ExecContext(ctx, stmt,
+		res.FirstName,
+		res.LastName,
+		res.Email,
+		res.Phone,
+		res.StartDate,
+		res.EndDate,
+		res.RoomID,
+		time.Now(),
+		time.Now(),
+	)
+	if err != nil {
+		return err
+	}
+	return nil
 }
