3cf82cb48cd1ba4391dff27281d3d3e53f95964e
Rajath <acharya.drajath@gmail.com>
2024-06-18 13:57:30 +0530

    117. Adding a migration for seeding rooms 118. Adding a migration for seeding restrictions

---
+++ migrations/20240618082159_seed_rooms_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.rooms (room_name,created_at,updated_at) VALUES
+	 ('Generals Quarters','2024-06-12 00:00:00.000','2024-06-12 00:00:00.000'),
+	 ('Major Suit','2024-06-24 00:00:00.000','2024-06-24 00:00:00.000');
