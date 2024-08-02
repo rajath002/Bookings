700265c3b3c0f18cb2e110e1026ba52f0a869c51
Rajath <acharya.drajath@gmail.com>
2024-08-02 11:25:55 +0530

    162. Handling Calendar changes IV

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -55,7 +55,7 @@ func routes(app *config.AppConfig) http.Handler {
 		mux.Get("/process-reservation/{src}/{id}", handlers.Repo.AdminProcessReservation)
 		mux.Get("/delete-reservation/{src}/{id}", handlers.Repo.AdminDeleteReservation)
 
-		mux.Get("/reservations/{src}/{id}", handlers.Repo.AdminShowReservations)
+		mux.Get("/reservations/{src}/{id}/show", handlers.Repo.AdminShowReservations)
 		mux.Post("/reservations/{src}/{id}", handlers.Repo.AdminPostShowReservation)
 
 	})
